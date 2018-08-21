ff71644fa6680fd1166173da2e92d3a32d3b0d8b
Ryan <rck1464@rit.edu>
2018-08-20 23:39:49 -0400

    Merge of HistoricalLeagues and Leagues components

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -3,13 +3,13 @@ import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
-import { League, Leagues, HistoricalLeagues } from './components/Leagues/index';
+import { League, Leagues } from './components/Leagues/index';
 import { Counter } from './components/Counter';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
     <Route path='/counter' component={Counter} />
     <Route exact path='/leagues/' component={Leagues} />
-    <Route exact path='/leagues/historical' component={HistoricalLeagues} />
-    <Route exact path='/leagues/:leagueName' component={League} />
+    <Route exact path='/leagues/:argument' component={Leagues} />
+    <Route exact path='/league/:leagueName' component={League} />
 </Layout>;
