a55294b63faf956a22a868c66041a701c7cd7065
Ryan <rck1464@rit.edu>
2018-10-02 00:31:33 -0400

    Fixed model naming

--- Models/Season.cs
+++ Models/Season.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace TagProLeague.Models
 {
-    public class MongoDbSeason
+    public class Season
     {
         [BsonId]
         public ObjectId Id { get; set; }
