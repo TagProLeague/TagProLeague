24d911f93abde42fa1bf4c5b733e30114d443dc2
Ryan <rck1464@rit.edu>
2018-10-17 02:55:11 -0400

    -Removed unused Controllers and Components -Removed HistoricalLeagues access -package update for AspNetCore & Bogus

--- ClientApp/components/Leagues/Leagues.tsx
+++ ClientApp/components/Leagues/Leagues.tsx
@@ -1,75 +1,44 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-import { Link } from 'react-router-dom';
 import 'isomorphic-fetch';
 
 interface LeaguesState {
     leagues: LeagueModel[];
     loading: boolean;
-    isHistoricalPage: boolean;
-}
-
-interface Params {
-    argument: string;
 }
 
 export class Leagues extends React.Component<RouteComponentProps<{}>, LeaguesState> {
     constructor(props: any) {
         super(props);
-        const params = this.props.match.params as Params;
+
         this.state = {
             leagues: [],
-            loading: true,
-            isHistoricalPage: false
+            loading: true
         };
 
         this.loadLeagues();
     }
 
     loadLeagues = () => {
-        fetch('api/Leagues/')
+        fetch('api/leagues/')
             .then(response => response.json() as Promise<LeagueModel[]>)
             .then(data => {
                 this.setState({
                     leagues: data,
-                    loading: false,
-                    isHistoricalPage: false
-                });
-            });
-    }
-
-    loadHistoricalLeagues = () => {
-        fetch('api/Leagues/Historical')
-            .then(response => response.json() as Promise<LeagueModel[]>)
-            .then(data => {
-                this.setState({
-                    leagues: data,
-                    loading: false,
-                    isHistoricalPage: true
+                    loading: false
                 });
             });
     }
 
     public render() {
-        var titleText = <h1>Leagues</h1>;
-        var bodyText = <p>This component fetches leagues.</p>
-        var link = <button onClick={this.loadHistoricalLeagues}>This link fetches historical leagues</button>;
-
-        if (this.state.isHistoricalPage)
-        {
-            titleText = <h1>Historical Leagues</h1>
-            bodyText = <p>This component fetches historical leagues.</p>
-            link = <button onClick={this.loadLeagues}>This link fetches active leagues</button>;
-        }
 
         let leagueTable = this.state.loading
             ? <p><em>Loading...</em></p>
             : Leagues.renderLeaguesTable(this.state.leagues);
        
         return <div>
-            {titleText}
-            {bodyText}
-            {link}
+            <h1>Leagues</h1>
+            <p>This component fetches leagues.</p>
             {leagueTable}
         </div>;
     }
